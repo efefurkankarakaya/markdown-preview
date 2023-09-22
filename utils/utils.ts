7a5096d35ca364ef1ebcde74bb568922f4835140
efefurkankarakaya <efkdev@outlook.com>
2023-09-22 19:45:33 +0300

    fix: line breaks are not allowed

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,27 +1,33 @@
 import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
+import remarkHtml, { type Options } from "remark-html";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
 
 export async function convertMarkdownToHTML(content: string): Promise<string> {
-  // Use remark to convert markdown into HTML string
-  // const processedContent = await remark().use(html).process("# Hello");
-  const processedContent = await unified().use(remarkParse).use(remarkRehype).use(rehypeSanitize).use(rehypeStringify).process(content);
-  const contentHTML = processedContent.toString();
+  const remarkHtmlConfig: Options = {
+    sanitize: false,
+  };
 
-  // Comiine the data with the id
+  // const processedContent = await unified().use(remarkParse).use(remarkRehype).use(rehypeSanitize).use(rehypeStringify).process(content);
+  const processedContent = await unified().use(remarkParse).use(remarkHtml, remarkHtmlConfig).process(content);
+  const contentHTML = processedContent.toString();
   return contentHTML;
 }
 
-export function combineClasses(initial: string, upcoming?: string) {
+export function combineClasses(initial: string, ...upcoming: string[]) {
   if (!initial) {
     throw new Error("Initial can't be falsy.");
   }
 
-  if (!upcoming) {
+  /* If the first element of upcoming is empty string, then assume that there's no upcoming style. */
+  if (!upcoming[0]) {
     return initial;
   }
 
-  return initial + " " + upcoming;
+  let combined = initial;
+  upcoming.forEach((style) => (combined += " " + style));
+
+  return combined;
 }
