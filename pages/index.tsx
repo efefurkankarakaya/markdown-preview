3386442348cebaf947b7038b4e59aea2ad471031
efefurkankarakaya <efkdev@outlook.com>
2023-10-02 00:31:16 +0300

    feat: add welcome text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import { FixedTextArea } from "@/components";
 import Style from "./index.module.css";
-import { type EventHandler, useState, FormEventHandler, useEffect } from "react";
+import { type EventHandler, useState, type FormEventHandler, useEffect } from "react";
 import { combineClasses, convertMarkdownToHTML } from "@/utils/utils";
+import { welcome } from "@/constants/welcome";
 
-const inter = Inter({ subsets: ["latin"] });
-
-/**
- * TODO:
- * Dockerfile
- * Tests (with Cypress, e2e and unit)
- * Workflow (run tests every commit)
- * Save / download or Copy
- * Set data to the cookies
- */
 export default function Home() {
-  const [markdownValue, setMarkdownValue] = useState<string>("");
+  const [markdownValue, setMarkdownValue] = useState<string>(welcome);
   const [htmlValue, setHTMLValue] = useState<string>("");
 
   const onChangeMarkdownValue: FormEventHandler<HTMLTextAreaElement> = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    console.log(event.target.value);
     setMarkdownValue(event.target.value);
   };
 
