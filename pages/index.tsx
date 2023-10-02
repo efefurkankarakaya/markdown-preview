839a042ad1a726359808c9b9f24c8688f330a508
efefurkankarakaya <efkdev@outlook.com>
2023-10-02 16:08:37 +0300

    style: remove eof config, use default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,55 @@
-import { FixedTextArea } from "@/components";import Style from "./index.module.css";import {  type EventHandler,  useState,  type FormEventHandler,  useEffect} from "react";import { combineClasses, convertMarkdownToHTML } from "@/utils/utils";import { welcome } from "@/constants/welcome";export default function Home() {  const [markdownValue, setMarkdownValue] = useState<string>(welcome);  const [htmlValue, setHTMLValue] = useState<string>("");  const onChangeMarkdownValue: FormEventHandler<HTMLTextAreaElement> = (    event: React.ChangeEvent<HTMLTextAreaElement>  ) => {    setMarkdownValue(event.target.value);  };  useEffect(() => {    convertMarkdownToHTML(markdownValue).then((data) => setHTMLValue(data));  }, [markdownValue]);  return (    <div className={Style.container}>      <div className={Style.innerContainer}>        <FixedTextArea          extraClassName={combineClasses(            Style.textArea,            Style.padding,            Style.margin,            Style.border          )}          textAreaProps={{            onChange: onChangeMarkdownValue,            value: markdownValue            // rows: 20,          }}        />        <div          className={combineClasses(            Style.outputArea,            Style.padding,            Style.margin,            Style.border          )}          dangerouslySetInnerHTML={{ __html: htmlValue }}          id="outputField"        />      </div>    </div>  );}
+import { FixedTextArea } from "@/components";
+import Style from "./index.module.css";
+import {
+  type EventHandler,
+  useState,
+  type FormEventHandler,
+  useEffect
+} from "react";
+import { combineClasses, convertMarkdownToHTML } from "@/utils/utils";
+import { welcome } from "@/constants/welcome";
+
+export default function Home() {
+  const [markdownValue, setMarkdownValue] = useState<string>(welcome);
+  const [htmlValue, setHTMLValue] = useState<string>("");
+
+  const onChangeMarkdownValue: FormEventHandler<HTMLTextAreaElement> = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setMarkdownValue(event.target.value);
+  };
+
+  useEffect(() => {
+    convertMarkdownToHTML(markdownValue).then((data) => setHTMLValue(data));
+  }, [markdownValue]);
+
+  return (
+    <div className={Style.container}>
+      <div className={Style.innerContainer}>
+        <FixedTextArea
+          extraClassName={combineClasses(
+            Style.textArea,
+            Style.padding,
+            Style.margin,
+            Style.border
+          )}
+          textAreaProps={{
+            onChange: onChangeMarkdownValue,
+            value: markdownValue
+            // rows: 20,
+          }}
+        />
+        <div
+          className={combineClasses(
+            Style.outputArea,
+            Style.padding,
+            Style.margin,
+            Style.border
+          )}
+          dangerouslySetInnerHTML={{ __html: htmlValue }}
+          id="outputField"
+        />
+      </div>
+    </div>
+  );
+}
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,1 +1,34 @@
-import remarkHtml, { type Options } from "remark-html";import remarkParse from "remark-parse";import { unified } from "unified";export async function convertMarkdownToHTML(content: string): Promise<string> {  const remarkHtmlConfig: Options = {    sanitize: false  };  // const processedContent = await unified().use(remarkParse).use(remarkRehype).use(rehypeSanitize).use(rehypeStringify).process(content);  const processedContent = await unified()    .use(remarkParse)    /* @ts-ignore */    .use(remarkHtml, remarkHtmlConfig)    .process(content);  const contentHTML = processedContent.toString();  return contentHTML;}export function combineClasses(initial: string, ...upcoming: string[]): string {  if (!initial) {    throw new Error("Initial can't be falsy.");  }  /* If the first element of upcoming is empty string, then assume that there's no upcoming style. */  if (!upcoming[0]) {    return initial;  }  let combined = initial;  upcoming.forEach((style) => (combined += " " + style));  return combined;}
+import remarkHtml, { type Options } from "remark-html";
+import remarkParse from "remark-parse";
+import { unified } from "unified";
+
+export async function convertMarkdownToHTML(content: string): Promise<string> {
+  const remarkHtmlConfig: Options = {
+    sanitize: false
+  };
+
+  // const processedContent = await unified().use(remarkParse).use(remarkRehype).use(rehypeSanitize).use(rehypeStringify).process(content);
+  const processedContent = await unified()
+    .use(remarkParse)
+    /* @ts-ignore */
+    .use(remarkHtml, remarkHtmlConfig)
+    .process(content);
+  const contentHTML = processedContent.toString();
+  return contentHTML;
+}
+
+export function combineClasses(initial: string, ...upcoming: string[]): string {
+  if (!initial) {
+    throw new Error("Initial can't be falsy.");
+  }
+
+  /* If the first element of upcoming is empty string, then assume that there's no upcoming style. */
+  if (!upcoming[0]) {
+    return initial;
+  }
+
+  let combined = initial;
+  upcoming.forEach((style) => (combined += " " + style));
+
+  return combined;
+}
