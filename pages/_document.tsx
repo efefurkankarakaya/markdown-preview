327d95a44b88591839b35db552b77a157fa18a47
efefurkankarakaya <efkdev@outlook.com>
2023-10-02 18:46:09 +0300

    feat: use `title` and `favicon.ico`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const AppName: string = "Markdown Preview";
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>{AppName}</title>
+        <link rel="icon" href="next-favicon.ico" sizes="any" />
+      </Head>
       <body>
         <Main />
         <NextScript />
